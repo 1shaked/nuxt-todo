aa76db7421521b3ef647732da70007b21df24ccb
shaked <shaked1hen@gmail.com>
2020-05-08 00:38:30 +0300

    change the server url

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://localhost:8000/'
+    baseURL: 'http://shaked.pythonanywhere.com/'
   },
   /*
   ** Build configuration
